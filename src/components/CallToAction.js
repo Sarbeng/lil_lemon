1b356445228e23e121076685f1ac9d3fc81bebc7
Sarbeng <sarbengbaafi@gmail.com>
2023-07-17 17:28:52 +0000

    cta started

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -3,8 +3,15 @@ export default function CallToAction() {
       
         
       
-     <section className=''>
-     CTA
+     <section className="bg-primary-green px-[290px] py-12 text-white flex">
+        <section id="content" className="w-[280px]">
+            <h1 className="font-markazi text-[64px] font-medium leading-10 text-primary-yellow">Little Lemon </h1>
+            <h3 className="font-markazi font-normal text-[40px]">Chicago</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </section>
+        <section id="image">
+            <img src="../assets/restauranfood.jpg" className="w-[375px]"/>
+        </section>
      </section>
      
     );
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,8 +7,8 @@ export default function LayoutPage (props) {
         <>
             <section className='flex justify-between items-center h-24 py-2 md:px-[290px] bg-white shadow-sm'><Header />
             <Nav /></section>
-            <main className='px-[290px]'>{props.children}</main>
-            <section className='px-[290px] bg-slate-100 py-16'>
+            <main className=''>{props.children}</main>
+            <section className='md:px-[290px] bg-slate-100 py-16'>
             <Footer />
             </section>
         </>
