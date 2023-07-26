f4e859d342578f7feb4ccce9e94750b5677bb867
Sarbeng <sarbengbaafi@gmail.com>
2023-07-26 16:00:21 +0000

    Our story section mobile view taken care of

--- src/components/pages/Home/OurStory.js
+++ src/components/pages/Home/OurStory.js
@@ -3,23 +3,23 @@ import mario_and_adrian_a from "./assets/Mario_and_Adrian_A.jpg"
 import mario_and_adrian_b from "./assets/Mario and Adrian b.jpg"
 export default function OurStory() {
     return (
-      
-        
-      
-    <section className="flex justify-center items-center h-[590px]">
-         <section className=" px-[290px] py-20 text-white flex justify-between gap-14">
-        <section id="content" className="w-1/2 flex flex-col gap-6">
-            <h1 className="font-markazi text-display font-medium  text-black ">Little Lemon </h1>
-            <h3 className="font-markazi font-normal text-[40px] text-black  -mt-12">Chicago</h3>
-            <p className="text-black">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
-Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. </p>
+
+
+
+        <section className="flex flex-col md:flex-row justify-center  md:h-[590px] h-auto mt-16 mb-96 md:mb-0">
+            <section className=" md:px-[290px] px-4 md:py-16 text-white flex flex-col md:flex-row justify-between gap-14">
+                <section id="content" className="w-1/2 flex flex-col gap-6">
+                    <h1 className="font-markazi md:text-display text-6xl w-screen md:w-full font-medium  text-primary-yellow   ">Little Lemon </h1>
+                    <h3 className="font-markazi font-normal text-[40px] text-highlights-black  -mt-12">Chicago</h3>
+                    <p className="text-highlights-black w-screen md:w-full">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. </p>
+                </section>
+                <section id="image" className=" w-1/2 flex flex-col md:flex-row  justify-start items-baseline">
+                    <img src={mario_and_adrian_a} alt="restaurant food" className="w-48 h-64 md:w-[268px] md:h-[336px] rounded-2xl absolute ml-36 object-cover object-right" />
+                    <img src={mario_and_adrian_b} alt="restaurant food" className=" w-48 h-64 md:w-[268px] md:h-[336px] rounded-2xl absolute  -z-20 mt-20 object-cover object-right-bottom" />
+                </section>
+            </section>
         </section>
-        <section id="image" className=" w-1/2 flex ">
-            <img src={mario_and_adrian_a} alt="restaurant food" className="w-[268px] h-[336px] rounded-2xl absolute ml-36 object-cover object-right"/>
-            <img src={mario_and_adrian_b} alt="restaurant food" className="w-[268px] h-[336px] rounded-2xl absolute  -z-20 mt-20 object-cover object-right-bottom"/>
-        </section>
-     </section>
-    </section>
-     
+
     );
 }
--- src/components/pages/Home/Testimonials.js
+++ src/components/pages/Home/Testimonials.js
@@ -27,7 +27,7 @@ export default function Testimonials() {
             review: 'Amazing Service'
         },
     ]
-    return <section className=" bg-primary-yellow px-4 md:px-[290px] mt-8 md:h-[590px] h-auto py-8 flex flex-col md:flex-row justify-center items-center">
+    return <section className=" bg-primary-yellow px-4 md:px-[290px] mt-8 md:h-[590px] h-auto py-8 flex flex-col  justify-center items-center">
         <h2 className="font-markazi text-display text-center">Testimonials</h2>
         <section id="testimonials" className="flex gap-8 justify-between mt-16 flex-col  md:flex-row">
             {testimonials.map((card) => {
