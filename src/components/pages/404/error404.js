85a2852491048aa2c9c2ac540a0e00a9d8afcde8
Sarbeng <sarbengbaafi@gmail.com>
2023-07-27 10:09:28 +0000

    404 page constructed

--- src/components/pages/404/error404.js
+++ src/components/pages/404/error404.js
@@ -1,7 +1,17 @@
+import {  MdOutlineErrorOutline, } from "react-icons/md";
+
 export default function Error404 () {
     return (
         <>
-            404
+           <section className="flex justify-center py-16 px-4 items-center gap-4 h-[400px]  text-primary-green flex-col">
+            <section className="flex gap-3 items-center">
+            <h1 className="text-4xl capitalize ">Sorry</h1>
+            <span className="text-4xl"><MdOutlineErrorOutline/></span>
+            </section>
+            <p>the page you'
+            re looking for does not exist!</p>
+           
+           </section>
         </>
     );
 }
--- src/components/pages/UnderContruction/UnderContruction.js
+++ src/components/pages/UnderContruction/UnderContruction.js
@@ -1,7 +1,12 @@
+import { MdOutlineConstruction } from "react-icons/md";
+
 export default function UnderConstruction () {
     return (
         <>
-            UnderConstruction
+           <section className="flex justify-center py-16 px-4 items-center gap-4 h-[400px]  text-primary-green">
+            <h1 className="text-2xl md:text-4xl capitalize ">Page Under Construction</h1>
+            <span className="text-4xl"><MdOutlineConstruction/></span>
+           </section>
         </>
     );
 }
