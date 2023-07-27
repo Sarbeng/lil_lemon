baee129715f438a97df0b8f4e6ab2244f7e06529
Sarbeng <sarbengbaafi@gmail.com>
2023-07-27 11:56:58 +0000

    booking page tailwind styling added

--- src/components/Layout/Nav.js
+++ src/components/Layout/Nav.js
@@ -40,13 +40,13 @@ export default function Nav() {
                 }
 
             </ul></section>
-            <button className="block md:hidden" onClick={handleToggle}>
+            <button className={isExpanded? '' : `block text-2xl md:hidden`} onClick={handleToggle}>
                 {isExpanded ? <MdOutlineClose /> :
                     <MdOutlineMenu />}
             </button>
             
         </nav>
-        <ul className={isExpanded ? `flex flex-col absolute   bg-white shadow-sm h-screen w-1/2 top-0 left-0 py-12 z-auto p-4 gap-12  text-highlight-text  items-right`: "hidden" } onClick={handleToggle}>
+        <ul className={isExpanded ? `flex flex-col absolute  bg-white shadow-sm h-screen w-1/2 top-0 left-0 py-12 z-auto p-4 gap-12  text-highlight-text  items-right`: " hidden" } onClick={handleToggle}>
                 {
                     links.map((navlink) => {
                         return <li className="capitalize w-full"><NavLink to={navlink.linkTo} className={({ isActive }) => isActive ? "border border-b-2 border-x-0 p-4 border-primary-yellow w-full " : "p-4 hover:border-primary-yellow hover:border hover:py-4  hover:border-b-2 hover:border-x-0 w-full"}>{navlink.text}</NavLink></li>
