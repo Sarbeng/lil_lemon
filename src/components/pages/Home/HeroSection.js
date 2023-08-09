a60b2d9b422caab987eb2a40532dbaca87b4b47c
Sarbeng <sarbengbaafi@gmail.com>
2023-08-09 18:03:37 +0000

    reserve table button navigation added

--- src/components/pages/Home/HeroSection.js
+++ src/components/pages/Home/HeroSection.js
@@ -1,4 +1,7 @@
+import { useNavigate } from 'react-router-dom';
 export default function HeroSection() {
+    const navigate = useNavigate();
+
     return (
       
         
@@ -8,7 +11,7 @@ export default function HeroSection() {
             <h1 className="font-markazi md:text-display text-6xl w-screen md:w-full font-medium text-primary-yellow">Little Lemon </h1>
             <h3 className="font-markazi font-normal text-[40px] md:-mt-12 -mt-8 text-4xl">Chicago</h3>
             <p className="w-screen md:w-full">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <button className="capitalize bg-primary-yellow text-lead-text  text-highlights-black rounded-[5px] h-[48px] px-8 py-3 w-[200px]">Reserve a table</button>
+            <button className="capitalize bg-primary-yellow text-lead-text  text-highlights-black rounded-[5px] h-[48px] px-8 py-3 w-[200px]" onClick={() => navigate('/booking')}>Reserve a table</button>
         </section>
         <section id="image" className="">
             <img src="../../../assets/restauranfood.jpg" alt="restaurant food" className="w-[375px] h-[420px] rounded-2xl"/>
